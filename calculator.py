16066caca879484440dc70c50d9e4bf5ce0c8d71
GHATAK-09 <tej231291@gmail.com>
2026-09-18 08:18:22 +0530

    Add simple calculator -  my first Python program

---
+++ calculator.py
@@ -0,0 +1,12 @@
+# My First Python Program - Simple Calculator
+# Hobby: Python Coding
+
+num1 = int(input("Pehla number daalo: "))
+num2 = int(input("Dusra number daalo: "))
+
+print("Jod:", num1 + num2)
+print("Ghatav:", num1 - num2)
+print("Guna:", num1 * num2)
+print("Bhaag:", num1 / num2)
+
+print("Banane wala: GHATAK-09")
