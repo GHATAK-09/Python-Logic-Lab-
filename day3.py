0ff2942077697f744d47350b5ce3e5b3ba33ebe3
GHATAK | Python Developer <tej231291@gmail.com>
2026-09-19 12:07:43 +0530

    Add Day 3 GHATAK Logic Test script

---
+++ day3.py
@@ -0,0 +1,16 @@
+# Day 3 - GHATAK Logic Test
+print("Jai Hind")
+
+# Test 1 - Running Test
+running_km = 2
+if running_km >= 1:
+    print("GHATAK Pass - Jai Hind")
+else:
+    print("Aur mehnat karni hai")
+
+# Test 2 - Age Test
+age = 18
+if age >= 18:
+    print("Python Coder")
+else:
+    print("Future Coder")
