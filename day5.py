2506ed6b69d4d52f7e5c1598a39c4756905b0499
GHATAK | Python Developer <tej231291@gmail.com>
2026-09-22 20:48:50 +0530

    Add ghatak_salaam function and roll call

---
+++ day5.py
@@ -0,0 +1,13 @@
+# Day 5 - Function - KARAN GHATAK
+
+def ghatak_salaam(name):
+    print("Jai Hind", name, "- GHATAK Ready Hai!")
+    print("Mission Continue...")
+
+# Roll Call
+print("--- Day 5 Roll Call ---")
+ghatak_salaam("Karan")
+print("")
+ghatak_salaam("Commando")
+print("")
+ghatak_salaam("Sher")
