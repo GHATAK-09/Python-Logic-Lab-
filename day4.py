4eb0caf47d47392253b9999256d5fbe3b7980388
GHATAK | Python Developer <tej231291@gmail.com>
2026-09-22 16:09:04 +0530

    Add Day 4 loop practice script

---
+++ day4.py
@@ -0,0 +1,10 @@
+# Day 4 - Loop - GHATAK Practice
+
+print("--- GHATAK Roll Call ---")
+for i in range(1, 6):
+    print("Jai Hind -", i)
+
+print("\n--- 2 ka Table ---")
+num = 2
+for i in range(1, 11):
+    print(num, "x", i, "=", num * i)
